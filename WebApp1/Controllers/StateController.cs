221b8ef61c2e7004343cca1b45599609f4d0f797
Christen <christen.foad@gmail.com>
2026-02-07 20:40:52 +0200

    Middleare -State managment

---
+++ WebApp1/Controllers/StateController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp1.Controllers
+{
+    public class StateController : Controller
+    {
+        
+        //Session store ==>get
+        public IActionResult setSession(string name,int age)
+        {
+            //logic
+            //store
+            HttpContext.Session.SetString("Name", name);
+            HttpContext.Session.SetInt32("Age", age);
+            return Content("Session Store Success");
+        }
+        public IActionResult GetSession()
+        {
+            //logic
+            //read info session
+            string n=HttpContext.Session.GetString("Name");
+            int? a =  HttpContext.Session.GetInt32("Age");
+            return Content($"Name={n}\t Age={a}");
+        }
+
+        public IActionResult SetCookie(string name,int age)
+        {
+            //server =response=>client
+            //session cookie expired with session
+            HttpContext.Response.Cookies.Append("Name", name);
+            //Presistent cookie "Expiration date"
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTimeOffset.Now.AddHours(1);
+
+            HttpContext.Response.Cookies.Append("Age", age.ToString(), options);
+            return Content("Cookie sucess Added");
+        }
+        public IActionResult GetCookie() {
+            //logic 
+            //name from coooie
+            string name=HttpContext.Request.Cookies["Name"];
+            string age=HttpContext.Request.Cookies["Age"];
+            return Content($"From Cookie name ={name}\t age={age}");
+        }
+
+    }
+}
--- WebApp1/Program.cs
+++ WebApp1/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using WebApp1.Models;
 using WebApp1.Repository;
@@ -19,29 +20,56 @@ namespace WebApp1
             builder.Services.AddDbContext<StepsContext>(dbContextOptionBuilder => {
                 dbContextOptionBuilder.UseSqlServer(builder.Configuration.GetConnectionString("cs"));  
             });
-
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(45);
+            });
             //Cusotm Service and Need To Register
             builder.Services.AddScoped<IEmployeeRepository,EmployeeRepository>();
             builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
             builder.Services.AddScoped<IService, Service>();//create object per request
             //Create Web app
             var app = builder.Build();
+            #region Custom PipleLine
+            //inline Midleware
+            //app.Use(async (httpcontext,nextMidleware) => {
+            //    await  httpcontext.Response.WriteAsync("1- Middleware 1\n");//<--1
+            //    //call next
+            //    await  nextMidleware.Invoke();//<--2
+            //    await httpcontext.Response.WriteAsync("1-1 Middleware 1-1\n");//<--7
+            //});
+            //app.Use(async (httpcontext, nextMidleware) => {
+            //    await httpcontext.Response.WriteAsync("2- Middleware 2\n");//<--3
+            //    //call next
+            //    await nextMidleware.Invoke();//<--4
+            //    await httpcontext.Response.WriteAsync("2-2 Middleware 2-2\n");//<--6
+
+            //});
+            //app.Run(async(httpContext) =>
+            //{
+            //    await httpContext.Response.WriteAsync("3- Terminate\n");//<--5
+
+            //});
+          
+            #endregion
 
             // Configure the HTTP request pipeline. middleware Day2
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseStaticFiles();
+            #region built in middlewares
+              if (!app.Environment.IsDevelopment())
+              {
+                  app.UseExceptionHandler("/Home/Error");
+              }
+              app.UseStaticFiles();//hanadel wwwroot
 
-            app.UseRouting();
+              app.UseRouting();
+                app.UseSession();
+                app.UseAuthorization();
 
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-            
+              app.MapControllerRoute(
+                  name: "default",
+                  pattern: "{controller=Home}/{action=Index}/{id?}");
+              
+            #endregion
             //run web app
             app.Run();
         }
